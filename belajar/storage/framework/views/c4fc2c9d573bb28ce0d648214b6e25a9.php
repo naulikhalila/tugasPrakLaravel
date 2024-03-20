847711a54e5fc41562824db3096e28747bb566b8
naulikhalila <151435542+naulikhalila@users.noreply.github.com>
2024-03-20 10:22:04 +0700

    Add files via upload

---
+++ belajar/storage/framework/views/c4fc2c9d573bb28ce0d648214b6e25a9.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nauli Web Page</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.
+css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css
+">
+    <link rel="stylesheet" href="assets/style/style.css">
+</head>
+
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle
+.min.js"></script>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+            <a class="navbar-brand" href="#"><img src="../img/n.png" alt="" width="40" height="40"></a>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#" style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-weight: bold;color: rgb(29, 25, 117);">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#" style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-weight: bold;color: rgb(29, 25, 117);">About Me</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#" style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-weight: bold;color: rgb(29, 25, 117);">Galery</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+    <div id="bs-icons" style="background-color: rgb(29, 25, 117);">
+        <!-- Container -->
+        <div class="container py-5 px-4">
+            <div class="row">
+                <div class="col-lg-6">
+                    <h2 class="display-5 mb-3" style="color: white;">Hi! I'm Nauli Khalila °˖✧◝(⁰▿⁰)◜✧˖°
+                    </h2>
+                    <p class="fs-5" style="color: white;">Hi! This is my personal website. My name is Nauli Khalila, but I am usually called Nauli. I am a second-year student at Telkom University Surabaya, majoring in Information Systems. I have several hobbies, including listening to music,
+                        watching movies, and taking random photos. Here, I will showcase some of the photos I have captured.</p>
+                </div>
+                <div class="col-lg-6 ">
+                    <img class="img-fluid " src="../img/NAU.jpg" width="500" height="500" alt="Bootstrap Icons ">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="galery" style="background-color: rgb(29, 25, 117);">
+        <div class="container text-center">
+            <div class="row row-cols-3">
+                <div class="col">
+                    <img src="../img/a.jpg" width="400" height="500" alt="">
+                </div>
+                <br>
+                <div class="col">
+                    <img src="../img/c.jpg" width="400" height="500" alt="">
+                </div><br>
+                <div class="col">
+                    <img src="../img/d.jpg" width="400" height="500" alt="">
+                </div>
+                <br>
+                <div class="col">
+                    <img src="../img/e.jpg" width="400" height="500" alt="">
+                </div>
+                <div class="col">
+                    <img src="../img/f.jpg" width="400" height="500" alt="">
+                </div>
+                <div class="col">
+                    <img src="../img/g.jpg" width="400" height="500" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer -->
+    <div id="footer" class="bg-light py-5">
+        <!-- Container -->
+        <div class="container py-5 px-4">
+            <div class="row">
+                <div class="col-lg-3 mb-5">
+                    <a href="#" class="logo text-decoration-none">
+                        <div class="d-flex">
+                            <img class="img-fluid" src="../img/n.png" alt="" style="width: 40px;">
+                            <div class="fs-5 ms-2 text-black">Hi! I'm Nauli Khalila. </div>
+                        </div>
+                    </a>
+                    <div class="mt-2 text-muted">
+                        <small>Hi! This is my personal website. My name is Nauli Khalila, but I am usually called Nauli. I am a second-year student at Telkom University Surabaya, majoring in Information Systems. I have several hobbies, including listening to music,
+                            watching movies, and taking random photos. Here, I will showcase some of the photos I have captured.</small>
+                    </div>
+                    <div class="mt-2 text-muted">
+                        <small><a href="#" class="text-black"></a>
+  <a href="#" class="text-black"></a></small>
+                    </div>
+                    <div class="mt-2 text-muted">
+                        <small></small>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-2 offset-lg-1 mb-5">
+                    <h5>About Me</h5>
+
+                </div>
+                <div class="col-6 col-lg-2 mb-5">
+                    <h5>Contacts</h5>
+                    <ul class="list-unstyled">
+                        <li class="mb-2"><a href="#">Email: naulikhalilas@gmail.com</a></li>
+                        <li class="mb-2"><a href="#">Instagram: @naulikhalilaa</a></li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+</body>
+
+</html><?php /**PATH C:\xampp\htdocs\belajar\resources\views/personal.blade.php ENDPATH**/ ?>
